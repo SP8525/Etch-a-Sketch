59b9613e376d450511145fcd69de21f18cb71b7c
SP85 <205866464+SP8525@users.noreply.github.com>
2025-05-02 13:48:27 -0700

    Draw boxes from defined grid size

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const gridEdge = 16;
+// Take the container from HTML //
+const container = document.getElementById("container");
+console.log(container);
 
-const gridSizeQuery = document.createElement("h3");
-gridSizeQuery.style.color = "black";
-gridSizeQuery.textContent = "Enter the size of the grid (max 100).";
-document.body.appendChild(gridSizeQuery);
+function drawBox() {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+}
 
-const container = document.createElement("div");
-container.style.cssText = "background: white; border: 3px solid black;";
-
-const box = document.createElement("img");
-for (i = 1; i <= gridEdge; i++) {
-    box.appendChild(container);
+function drawGrid() {
+    for (i = 1; i <= 16; i ++) {
+        drawBox();
+    }
 }
 
 
-document.body.appendChild(container);
 
+
+// // Create individual box(es) //
+// function drawBox () {
+//     const box = document.createElement("div");
+//     box.style.width = '100px';
+//     box.style.height = '100px';
+//     box.style.border = '1px solid black';
+//     container.appendChild(box);
+// } 
+
+// function createGrid (gridSizeQuery) {
+//     for (i = 1; i <= gridSizeQuery; i ++) {
+//         drawBox();
+//     }
+// }
+
+
+
+
+
+// // Adding container to page //
+// document.body.appendChild(container);
+
+// createGrid(gridSizeQuery);
