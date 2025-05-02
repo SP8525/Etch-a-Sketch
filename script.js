edcdbdb6ce33070bd586a23365ced420289a3682
SP85 <205866464+SP8525@users.noreply.github.com>
2025-05-02 16:04:27 -0700

    Add hovering with color

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
 // Take the container from HTML //
+gridSide =10;
+
 const container = document.getElementById("container");
 console.log(container);
 
 function drawBox() {
     const box = document.createElement("div");
     box.classList.add("box");
+    box.style.flex = `0 0 ${100 / gridSide}%`;
+    box.style.aspectRatio = "1 / 1";
     container.appendChild(box);
 }
 
 function drawGrid() {
-    for (i = 1; i <= 16; i ++) {
-        drawBox();
+    container.replaceChildren();
+    for (i = 0; i < gridSide * gridSide; i ++) {
+    drawBox();
     }
 }
 
+drawGrid();
 
-
-
-// // Create individual box(es) //
-// function drawBox () {
-//     const box = document.createElement("div");
-//     box.style.width = '100px';
-//     box.style.height = '100px';
-//     box.style.border = '1px solid black';
-//     container.appendChild(box);
-// } 
-
-// function createGrid (gridSizeQuery) {
-//     for (i = 1; i <= gridSizeQuery; i ++) {
-//         drawBox();
-//     }
-// }
-
-
-
-
-
-// // Adding container to page //
-// document.body.appendChild(container);
-
-// createGrid(gridSizeQuery);
+document.querySelectorAll(".box").forEach(boxEl => {
+    boxEl.addEventListener("mouseover", () => {
+        boxEl.style.backgroundColor = "red";
+    });
+});
